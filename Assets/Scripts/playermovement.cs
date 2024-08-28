0cc749c58c446edd02c470daa502997b22b98c75
JonathanZ <125500254+Zniver4@users.noreply.github.com>
2024-08-28 16:11:49 -0600

    intento de pared invisible

--- Assets/Scripts/playermovement.cs
+++ Assets/Scripts/playermovement.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class playermovement : MonoBehaviour
 {
     float horizontalMove = 0;
-
     public float runSpeedHorizontal = 5;
     public float runSpeed = 5;
 
@@ -23,3 +22,4 @@ public class playermovement : MonoBehaviour
         transform.position += new Vector3(horizontalMove, 0, 0) * Time.deltaTime * runSpeed;
     }
 }
+
