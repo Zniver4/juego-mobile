9a17856691cccdadbeab1dcda64dc5b3631085ab
Zniver4 <jonathan4zacos@gmail.com>
2024-09-11 10:11:12 -0600

    cosas arregladas y menus

--- Assets/Scripts/ZonaPerdida.cs
+++ Assets/Scripts/ZonaPerdida.cs
@@ -3,13 +3,16 @@ using UnityEngine;
 public class ZonaPerdida : MonoBehaviour
 {
     public GameObject canvasPerdido; // Asigna el canvas desde el editor
+    public GameObject canvasPausa; // Asigna el canvas de pausa desde el editor
 
     void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            canvasPerdido.SetActive(true); // Activa el canvas
+            canvasPerdido.SetActive(true); // Activa el canvas de perdido
+            canvasPausa.SetActive(false); // Desactiva el canvas de pausa
             Time.timeScale = 0; // Pausa el juego
         }
     }
 }
+
